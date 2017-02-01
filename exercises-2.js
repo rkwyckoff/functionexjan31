cc84c11e107653112b96946c2000ce2af73b7eaa
Kurt Wyckoff <kurtitservices@gmail.com>
2017-02-01 11:57:16 -0500

    Corrections

--- exercises-2.js
+++ exercises-2.js
@@ -33,6 +33,13 @@ function isVowel(char) {
 // ---------------------
 // Write a function translate() that will translate a text into "Rovarspraket". That is, double every consonant and place an occurrence of "o" in between. For example, translate("this is fun") should return the string "tothohisos isos fofunon".
 // ---------------------
+function isVowel(char) {
+    if (char === "a" || char === "e" || char === "i" || char === "o" || char === "u") {
+    return true;
+}   else {
+    return false;
+    }
+}
 
 function translate(text) {
   var output = "";
@@ -53,16 +60,6 @@ function translate(text) {
 // Define a function reverse() that computes the reversal of a string. For example, reverse("jag testar") should return the string "ratset gaj".
 // ---------------------
 
-function translate(text) {
-  var output = "";
-  for (var i = 0; i < text.length; i++) {
-    if (isVowel(text[i])) {
-      output += text[i]
-    } else {
-      output += text[i] + "o" + text[i];
-    }
-  }
-  return output;
 
   function reverseString(text) {
     var output = "";
